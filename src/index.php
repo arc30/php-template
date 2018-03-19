0bb4450ab531d5d3fdf3060c5b1a6524b66cc66c
Archana N <arc30@users.noreply.github.com>
2018-03-19 20:57:11 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,21 @@
 <?php
-phpinfo();
-?>
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo "Error creating database: " . $conn->connect_error;
+} else {
+    echo "Database created successfully";
+}
+
+$conn->close();
+?> 
